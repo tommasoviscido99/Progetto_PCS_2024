12e1c82f8685597d1ec78a4c16c2fb2b44145204
Tommaso Viscido <tommasoviscido@MacBook-Air-di-Tommaso.local>
2024-07-12 22:41:26 +0200

    Commit

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,7 +1,24 @@
+#include "fracture.hpp"
 #include <iostream>
+#include <vector>
+#include "fracture_tests.hpp"
+#include <gtest/gtest.h>
 
-int main()
-{
-    std::cout << "prova" << std::endl;
-    return 0;
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    string inputFilename = "/Users/tommasoviscido/Downloads/Progetto_PCS_2024/Project/DFN/FR3_data.txt";
+    vector<Fracture> fractures = readFractures(inputFilename);
+
+    vector<Trace> traces = computeTraces(fractures);
+    string outputFilename = "traces.txt";
+    writeTraces(traces, outputFilename);
+
+    string outputClassifiedFilename = "output.txt";
+    classifyAndWriteTracesByFracture(fractures, traces, outputClassifiedFilename);
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
 }
